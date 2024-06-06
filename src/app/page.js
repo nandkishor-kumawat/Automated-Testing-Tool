062e6e99e1be913fe6fa54c96ff088e5c5f36212
Somya Gupta <somyagupta7521@gmail.com>
2024-06-06 17:27:05 +0530

    add service card

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,39 @@
-import ExcelToJson from '@/components/excel-to-json'
+import ServiceCard from '@/components/service-card';
 import React from 'react'
 
+const data = [
+  { name: "Legal Entity Type", id: "LegalEntityType" },
+  { name: "Legal Entity Type Detail", id: "LegalEntityTypeDetail" },
+  { name: "Role", id: "Role" },
+  { name: "Feature", id: "Feature" },
+  { name: "Subscription", id: "Subscription" },
+  { name: "Subscription Configuration", id: "SubscriptionConfiguration" },
+  { name: "Feature Role Permission", id: "FeatureRolePermission" },
+  { name: "Department", id: "Department" },
+  { name: "Legal Entity", id: "LegalEntity" },
+  { name: "Legal Entity Address Mapping", id: "LegalEntityAddressMapping" },
+  { name: "User", id: "User" }
+]
+
+
 const page = () => {
+
+
+
   return (
     <div>
-      <ExcelToJson />
+      <div className="flex flex-center justify-center items-center mt-4 font-bold">
+        <h1>Automated Test Tool</h1>
+      </div>
+
+
+
+
+      <div className='max-w-lg m-auto'>
+        <div className='space-y-2 my-2'>
+        {data.map((service, i) => <ServiceCard key={i} data={service} />)}
+        </div>
+      </div>
     </div>
   )
 }
