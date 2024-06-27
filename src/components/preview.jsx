4970fe173de4d1fb81659f0aa56809309efda192
Nandkishor Kumawat <nandkishor41071@gmail.com>
2024-06-27 18:17:04 +0530

    update fix

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -19,11 +19,11 @@ const Preview = ({
   isVisible,
   handleClose
 }) => {
-  const sheetNames = Object.keys(fileData);
   const { fileData, selectedTables } = useTableStore();
   const [currentKey, setCurrentKey] = useState('');
-  const [obj, setObj] = useState();
+  const sheetNames = Object.keys(fileData);
 
+  const [obj, setObj] = useState();
   const [result, setResult] = useState([])
 
   useEffect(() => {
@@ -96,10 +96,11 @@ const Preview = ({
                       return (
                         <div
                           className={`flex gap-2 items-center justify-between w-full text-left ${!sheet && 'hover:bg-foreground/10 cursor-pointer'} transition-all duration-200 ease-in-out ${sheetName === currentKey && "bg-foregrond/20"}`}
+                          key={i}
                         >
                           <button
                             disabled={sheet}
-                            key={i} title={sheetName}
+                            title={sheetName}
                             className='px-1 py-2 flex-1 text-left'
                             onClick={() => { linkup(sheetName) }}>
                             <span className='flex-grow'>{sheetName}</span>
