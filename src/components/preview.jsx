e1c4b93af42eedd59ce193430f487c069c65fa57
Nandkishor Kumawat <nandkishor41071@gmail.com>
2024-06-27 12:25:44 +0530

    update preview

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -37,24 +37,32 @@ const Preview = ({
       <DialogContent className="max-w-5xl p-0 border-none">
         <div className='flex justify-center items-center w-full'>
           <div className='flex flex-row bg-gray-900 h-[90vh] w-full m-auto rounded-md overflow-hidden'>
-            <div className='flex flex-col bg-gray-500 px-2 py-4 gap-3 max-w-[300px] h-full w-full justify-between'>
-              <ScrollArea className='px-3'>
+            <div className='flex flex-col bg-gray-500 pl-2 py-4 gap-3 max-w-[600px] h-full w-full justify-between'>
 
-                <div className='divide-y divide-gray-400 h-full flex-1'>
-                  {selectedTables.map((t, i) => (
-                    <p key={i} className={`flex gap-2 items-center justify-between px-1 py-2 hover:bg-foreground/10 cursor-pointer transition-all duration-200 ease-in-out ${t.id === currentKey && "bg-foreground/20"}`} onClick={() => setCurrentKey(t.id)}>
-                      <span className='flex-grow'>{t.name}</span>
-                      <span className='text-sm text-blue-300'>{fileData[t.id]?.length ?? 0}</span>
-                    </p>
-                  ))}
-                  {/* {sheetNames.sort().map((t, i) => (
-                    <p key={i} className={`flex gap-2 items-center justify-between px-1 py-2 hover:bg-foreground/10 cursor-pointer transition-all duration-200 ease-in-out ${t === currentKey && "bg-foreground/20"}`} onClick={() => setCurrentKey(t)}>
-                      <span className='flex-grow'>{t}</span>
-                      <span className='text-sm text-blue-300'>{fileData[t]?.length ?? 0}</span>
-                    </p>
-                  ))} */}
-                </div>
-              </ScrollArea>
+              <div className="flex flex-auto w-full overflow-hidden">
+                <ScrollArea className='flex-1 pr-3'>
+                  <div className='divide-y divide-gray-400 w-full'>
+                    {selectedTables.map((t, i) => (
+                      <p key={i} title={t.name} className={`flex gap-2 items-center justify-between px-1 py-2 hover:bg-foreground/10 cursor-pointer transition-all duration-200 ease-in-out ${t.id === currentKey && "bg-foreground/20"}`} onClick={() => setCurrentKey(t.id)}>
+                        <span className='flex-grow'>{t.name}</span>
+                        {/* <span className='text-sm text-blue-300'>{fileData[t.id]?.length ?? 0}</span> */}
+                      </p>
+                    ))}
+                  </div>
+                </ScrollArea>
+
+                <ScrollArea className='flex-1 pr-3'>
+                  <div className='divide-y divide-gray-400 w-full'>
+                    {sheetNames.sort().map((t, i) => (
+                      <p key={i} title={t} className={`flex gap-2 items-center justify-between px-1 py-2 hover:bg-foreground/10 cursor-pointer transition-all duration-200 ease-in-out ${t === currentKey && "bg-foreground/20"}`} onClick={() => setCurrentKey(t)}>
+                        <span className='flex-grow'>{t}</span>
+                        {/* <span className='text-sm text-blue-300'>{fileData[t]?.length ?? 0}</span> */}
+                      </p>
+                    ))}
+                  </div>
+                </ScrollArea>
+              </div>
+
               <div className="w-full px-3 flex gap-2 flex-wrap">
                 <UploadConfirmationModal fileData={fileData} handleClosePreviewModal={handleClose} />
                 <Button variant={"destructive"} className='flex-auto' onClick={handleClose}>Close</Button>
